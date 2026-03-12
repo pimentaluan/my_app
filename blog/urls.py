e9eae8e9955f5e0fd80e9188cad4dc0c260097ab
pimentaluan <pimentaluan2005@gmail.com>
2026-03-12 17:59:53 -0300

    pratica 3

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns=[
     path('welcome/', views.welcome),
     path('eco/<mensagem>', views.eco),
-    path('info/', views.info)
+    path('info/', views.info),
+    
+    path("home/", views.home, name="home"),
+    path("contato/<int:numero>", views.contato, name="contato")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from datetime import date
 
 def welcome(request):
-    return HttpResponse('Bem-vindo ao meu blog!')
+    nome = "Luan"
+    data = date.today()
+    is_logged_in = True
+    role = 'admin'
+    produtos = [
+        {"nome": "Iphone", "preço": 10000},
+        {"nome": "Macbook", "preço": 12000},
+        {"nome": "Dolphin Mini", "preço": 120000},
+    ]
+    
+    numero = 988888888
+
+    context={
+        "nome": nome,
+        "data": data,
+        "is_logged_in":is_logged_in,
+        "role": role,
+        "produtos": produtos,
+        "numero": numero
+    }
+    return render(request, "blog/welcome.html", context)
 
 def eco(request, mensagem):
     return HttpResponse(f'Você digitou: {mensagem}')
@@ -10,3 +31,12 @@ def eco(request, mensagem):
 def info(request):
     info = {"disciplina": "RAD", "framework": "Django", "semestre": "2025.2"}
     return JsonResponse(info)
+
+def home(request):
+    return render(request, "blog/home.html")
+
+def contato(request, numero):
+    context = {
+        "numero": numero,
+    }
+    return render(request, "blog/contato.html", context)
